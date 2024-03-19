765e174fb6c9f21126051907c363d7f99b39be5d
Alemlg93 <162334560+Alemlg93@users.noreply.github.com>
2024-03-19 16:40:30 +0100

    Figuras

---
+++ Figuras/src/modelos/Model/Rectángulo.java
@@ -0,0 +1,87 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+private class Rectángulo {
+
+    /**
+     * Default constructor
+     */
+    private Rectángulo() {
+    }
+
+    /**
+     * 
+     */
+    private int base;
+
+    /**
+     * 
+     */
+    private int altura;
+
+    /**
+     * @param base 
+     * @param altura
+     */
+    public void Rectángulo(int base, int altura) {
+        // TODO implement here
+    }
+
+    /**
+     * @param base
+     */
+    public void setBase(int base) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getBase() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param altura
+     */
+    public void setAltura(int altura) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getAltura() {
+        // TODO implement here
+    }
+
+    /**
+     * base * altura
+     * @return
+     */
+    public int getArea() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * Devuelve el perímetro del rectángulo según la fórmula perímetro= 2*base + 2*altura
+     * @return
+     */
+    public int getPerímetro() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+}
